35b95b7759f641d605e17bd1015bee01a72feeb7
pjm349@drexel.edu <pjm349@drexel.edu>
2020-02-22 23:28:37 -0500

    Nevermind

--- src/agents/peterMangelsdorf/MarioTasks/AttackJump.java
+++ src/agents/peterMangelsdorf/MarioTasks/AttackJump.java
@@ -23,6 +23,7 @@ public class AttackJump extends Action {
 	@Override
 	public Status Execute(ActionState action, MarioForwardModel model) {
 		action.value = sequence.get(counter);
+		System.out.println("BOING");
 		counter += 1;
 		if (counter >= sequence.size())
 		{
@@ -33,9 +34,11 @@ public class AttackJump extends Action {
 	
 	private ArrayList<boolean[]> makeSequence() {
 		sequence = new ArrayList<>();
+		// Slow Down
 		sequence.add(new boolean[] {false, true, false, false, false});
 		sequence.add(new boolean[] {false, true, false, false, false});
 		sequence.add(new boolean[] {false, true, false, false, false});
+		// Squish Im!
 		sequence.add(new boolean[] {false, true, false, false, true});
 		sequence.add(new boolean[] {false, true, false, false, true});
 		sequence.add(new boolean[] {false, true, false, false, true});
