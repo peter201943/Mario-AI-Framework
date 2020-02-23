cbc962d99d07640ae526099aa15db198dfebf0d5
pjm349@drexel.edu <pjm349@drexel.edu>
2020-02-22 23:21:05 -0500

    Add new pit feature, need to test

--- src/agents/peterMangelsdorf/Agent.java
+++ src/agents/peterMangelsdorf/Agent.java
@@ -59,9 +59,12 @@ public class Agent implements MarioAgent {
     
     private Container makeBT() {
     	
+    	
     	// Declarations -- Correct Order
     	Container newBT = new Do();
 	    	Choose baseChoose = new Choose();
+	    	
+	    		// Enemy Countermeasures
 		    	Container doOne = new Do();
 		    		Condition enemiesClose = new EnemiesClose();
 		    		Container secondChoose = new Choose();
@@ -69,12 +72,22 @@ public class Agent implements MarioAgent {
 		    				Condition enemiesInRange = new EnemiesInRange();
 		    				Action attackJump = new AttackJump();
 		    			Action wait = new Wait();
+    			
+    			// Wall Countermeasures
 		    	Container doTwo = new Do();
 			    	Condition stopped = new Stopped();
 			    	Action rightRunJump = new RightRunJump();
+			    	
+		    	// Pit Countermeasures
+		    	Container doFour = new Do();
+		    		Condition pit = new Pit();
+		    		Action rightRunJump2 = new RightRunJump();
+		    	
+		    	// Time Countermeasures
 		    	Container doThree = new Do();
 		          	Action runRight2 = new RunRight();
 
+		          	
 		    		// Insertions -- Reverse Order
 		    		doOne.Add(enemiesClose);
 		    				secondDo.Add(enemiesInRange);
@@ -83,15 +96,53 @@ public class Agent implements MarioAgent {
 		    			secondChoose.Add(wait);
 		    		doOne.Add(secondChoose);
 		    	baseChoose.Add(doOne);
+		    	// Enemy Countermeasures
+		    	
 		    		doTwo.Add(stopped);
 		    		doTwo.Add(rightRunJump);
 		    	baseChoose.Add(doTwo);
+		    	// Wall Countermeasures
+		    	
+		    		doFour.Add(pit);
+		    		doFour.Add(rightRunJump2);
+		    	baseChoose.Add(doFour);
+		    	// Pit Countermeasures
+		    	
 		    		doThree.Add(runRight2);
 		    	baseChoose.Add(doThree);
+		    	// Time  Countermeasures
+		    	
     	newBT.Add(baseChoose);
  
+    	
     	// Finished!
     	return newBT;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
